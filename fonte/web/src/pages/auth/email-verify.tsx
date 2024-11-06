5d83040e6cdb5e29bd1099bd1258a499836d7702
Diogo Sobral <dsforasteiro@gmail.com>
2024-11-06 15:55:35 -0300

    Email verify added

--- fonte/web/src/pages/auth/email-verify.tsx
+++ fonte/web/src/pages/auth/email-verify.tsx
@@ -1,5 +1,29 @@
+import { useMutation } from '@tanstack/react-query'
+import { useEffect } from 'react'
+import { useNavigate, useSearchParams } from 'react-router-dom'
+
+import { emailVerify } from '../../api/email-verify'
+
 export function EmailVerify() {
-  return(
-    <h1>EmailVerify</h1>
-  )
+  const [searchParams] = useSearchParams()
+  const navigate = useNavigate()
+
+  const tokenId = searchParams.get('token')
+
+  const { mutateAsync: emailVerifyFn } = useMutation({
+    mutationKey: ['email-verify'],
+    mutationFn: emailVerify,
+    onSuccess: () => {
+      navigate('/sign-in?email-verified=true')
+    },
+    retry: false,
+  })
+
+  useEffect(() => {
+    if (tokenId) {
+      emailVerifyFn({ tokenId })
+    }
+  }, [tokenId])
+
+  return null
 }
